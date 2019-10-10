6b754648d0f77bbcef7034acc3a9d17c11d49dfb
Pedro Santiago - a20687 <56195962+a20687-iscteiul@users.noreply.github.com>
2019-10-10 11:57:32 +0100

    Ficha1.5 (#1)
    * Primeiro projecto Java para partilha!
    
    * First java project to commit
    
    * sffkjdsdb
    
    * Testing
    
    * again...

---
+++ Ficha1/src/TestingGitPedro.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TestingGitPedro {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner x = new Scanner(System.in);
+		System.out.println("Nome: ");
+		String nome = x.nextLine();
+		System.out.println("Bem-vindo " + nome + "!");
+		x.close();
+		
+	}
+	
+}
